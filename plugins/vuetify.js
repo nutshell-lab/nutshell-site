5adfc6506c428568b96ded03471ddb49db07e1c3
Thanith <nathan.boutte@viacesi.fr>
2019-02-21 14:24:47 +0100

    [theme] updating colors

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,10 +4,10 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#662773',
-    accent: '#F39200',
-    secondary: '#FFDD00',
-    whitish: '#FFFCE5',
+    primary: '#5e2e70',
+    accent: '#fddd3a',
+    secondary: '#e69632',
+    white: '#fffdef',
     info: colors.teal.lighten1,
     warning: colors.amber.base,
     error: colors.deepOrange.accent4,
