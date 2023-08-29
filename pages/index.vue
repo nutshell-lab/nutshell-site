39ef6b8cf22f6819e5faf4db319c9f55da5caed2
Victor Crépin <victor@nutshell-lab.com>
2023-08-29 22:10:38 +0200

    svgs

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="w-full h-full bg-zinc-950 flex items-center">
-    <nu-title class="mx-auto" text="Nutshell" active />
+  <div class="w-full h-full flex flex-col bg-zinc-950">
+    <div class="w-full h-16 flex justify-end px-10 py-2">
+      <img class="block" src="/burger.svg" width="48" height="48" />
+    </div>
+    <div class="w-full grow flex items-center justify-center">
+      <img class="block" src="/nutshell.svg" width="300" height="100" />
+    </div>
+    <div class="w-full flex flex-col items-center justify-center gap-4 my-8">
+      <span class="text-white text-lg">Un studio de developpement sur mesure.</span>
+      <img class="block animate-bounce" src="/arrow.svg" width="48" height="48" />
+    </div>
   </div>
 </template>
