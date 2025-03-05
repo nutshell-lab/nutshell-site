b920fd67979e09375383604db52501481e93b653
Thanith <nathan.boutte@viacesi.fr>
2025-03-05 18:15:39 +0100

    feat: add webdesign page

---
+++ pages/identite-de-marque.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="bg-alabaster font-inter">
+    /identite-de-marque
+	</div>
+</template>
--- pages/webdesign.vue
+++ pages/webdesign.vue
@@ -1,5 +1,260 @@
 <template>
 	<div class="bg-alabaster font-inter">
-    /devis
+    <section>
+      <h1>Agence Digitale de webdesign</h1>
+			<div>Donnez vie à votre site web en confiant votre projet à Nutshell, votre agence de création spécialisée en webdesign !</div>
+      <div>
+        Un bon webdesign est indispensable pour capter l’attention et affirmer votre identité digitale. Un design efficace améliore l’expérience utilisateur et renforce la confiance de vos visiteurs. Grâce à une conception de maquette adaptée à votre marché, vos supports web reflètent votre univers tout en optimisant vos performances en ligne.
+      </div>
+      <nu-link label="Demander un devis" to="/devis">
+        <nu-button class="w-full mt-auto">
+          Estimer mon projet
+        </nu-button>
+      </nu-link>
+    </section>
+		<section>
+			<h2>Tout le monde parle de Webdesign</h2>
+			<div>
+				Chaque page, qu’il s’agisse d’un site web, d’une landing page ou d’une application mobile, mérite un webdesign efficace et attractif. Un design bien structuré et une ergonomie pensée pour l’utilisateur permettent d’optimiser l’expérience et de maximiser l’engagement sur vos supports digitaux
+			</div>
+			<div>
+				Structurer, harmoniser et optimiser : c’est le rôle d’une agence webdesign. Nutshell vous accompagne dans la conception de supports web percutants, alliant design et performance pour maximiser l’engagement
+			</div>
+			<ul>
+				<li>Crédibilité et image de marque</li>
+				<li>Conversion et engagement</li>
+				<li>Adaptabilité et accessibilité</li>
+				<li>Impact sur le référencement naturel (SEO)</li>
+				<li>Différenciation concurrentielle</li>
+				<li>Alignement avec la stratégie digitale</li>
+			</ul>
+		</section>
+    <section>
+      <h2>Nos prestations de webdesign, élaborés pour votre succès.</h2>
+			<div>Un design impactant est essentiel pour capter l’attention et convertir vos visiteurs en clients. Notre agence de création conçoit des supports web ergonomiques, accessibles et engageants pour offrir une expérience utilisateur optimale.</div>
+			<div>
+        <div>
+          <h3>UX Design</h3>
+          <div>Nous créons des interfaces intuitives et performantes, centrées sur l’utilisateur, pour optimiser l’engagement et la satisfaction sur vos supports digitaux.</div>
+          <!-- <nu-link label="Découvrir notre agence sur mesure" to="/creation-site-internet/sur-mesure">
+            <nu-button class="w-full mt-auto">
+              Agence sur mesure 
+            </nu-button>
+          </nu-link> -->
+        </div>
+        <div>
+          <h3>UI Design</h3>
+          <div>Nous concevons des interfaces utilisateur esthétiques et cohérentes, en phase avec votre identité de marque, pour offrir une expérience visuelle fluide et engageante sur vos supports digitaux..</div>
+          <!-- <nu-link label="Découvrir notre agence sur mesure" to="/creation-site-internet/sur-mesure">
+            <nu-button class="w-full mt-auto">
+              Agence sur mesure 
+            </nu-button>
+          </nu-link> -->
+        </div>
+        <div>
+          <h3>Landing page</h3>
+          <div>Nous créons des landing pages optimisées, esthétiques et stratégiques, conçues pour capter l’attention de vos visiteurs et maximiser vos conversions.</div>
+          <!-- <nu-link label="Découvrir notre agence sur mesure" to="/creation-site-internet/sur-mesure">
+            <nu-button class="w-full mt-auto">
+              Agence sur mesure 
+            </nu-button>
+          </nu-link> -->
+        </div>
+        <div>
+          <h3>A/B testing</h3>
+          <div>Nous réalisons des tests A/B pour comparer différentes versions de vos supports web et optimiser l’expérience utilisateur, afin de maximiser vos conversions et performances digitales.</div>
+          <!-- <nu-link label="Découvrir notre agence sur mesure" to="/creation-site-internet/sur-mesure">
+            <nu-button class="w-full mt-auto">
+              Agence sur mesure 
+            </nu-button>
+          </nu-link> -->
+        </div>
+        <div>
+          <h3>Supports Digitaux Efficaces pour OF</h3>
+          <div>Nous concevons des programmes de formation et des certificats de participation, pour offrir des expériences pédagogiques interactives et engageantes.</div>
+          <!-- <nu-link label="Découvrir notre agence sur mesure" to="/creation-site-internet/sur-mesure">
+            <nu-button class="w-full mt-auto">
+              Agence sur mesure 
+            </nu-button>
+          </nu-link> -->
+        </div>
+      </div>
+    </section>
+    <section>
+      <h2>Webdesign et marketing digital : un duo incontournable pour la réussite de votre stratégie.</h2>
+			<div>Un bon webdesign n'est pas seulement esthétique : il est stratégique. Il a un impact direct sur la performance de vos campagnes marketing en améliorant l'expérience utilisateur et en augmentant l'efficacité de vos supports web. Avec un design cohérent et un parcours utilisateur optimisé, vous pouvez capter plus de visiteurs, les engager davantage et convertir plus de leads. En alignant votre webdesign avec votre stratégie digitale, vous maximisez l’impact de vos actions marketing tout en renforçant la fidélité de vos clients.</div>
+			<div>
+        <div>
+          <h3>Renforcer l’image de marque</h3>
+          <div>Concevoir un site web qui reflète les valeurs, l’identité et le ton de la marque, contribuant ainsi à une présence cohérente et forte sur internet.</div>
+          <!-- <div>Découvrir notre <nu-link label="Découvrir notre pole SEO" to="/referencement-naturel">pole Référencement Naturel</nu-link></div> -->
+        </div>
+        <div>
+          <h3>Améliorer le référencement naturel (SEO)</h3>
+          <div>Créer un design qui optimise la vitesse de chargement du site, la structure du contenu et l'ergonomie mobile, en respectant les bonnes pratiques SEO pour améliorer le classement sur les moteurs de recherche.</div>
+          <!-- <div>Explorer notre <nu-link label="Découvrir notre pole Création de Marque" to="/creation-de-marque">pole Création de Marque</nu-link></div> -->
+        </div>
+        <div>
+          <h3>Augmenter l’engagement et la fidélité des utilisateurs</h3>
+          <div>Utiliser un design attrayant et interactif pour captiver l’attention des utilisateurs, les inciter à passer plus de temps sur le site et favoriser un retour fréquent.</div>
+          <!-- <div>Voir notre <nu-link label="Découvrir notre pole SEO" to="/strategie-digitale">pole Stratégie Digitale</nu-link></div> -->
+        </div>
+        <div>
+          <h3>Améliorer l’accessibilité</h3>
+          <div>Concevoir un site accessible à tous, y compris aux personnes en situation de handicap, en suivant les normes d’accessibilité web (WCAG).</div>
+        </div>
+        <div>
+          <h3>Se distinguer de la concurrence</h3>
+          <div>Offrir une interface et une expérience unique qui se démarquent de vos concurrents et attirent l’attention des visiteurs.</div>
+          <!-- <div>Découvrir notre <nu-link label="Découvrir notre pole Webdesign" to="/webdesign">pole Webdesign</nu-link></div> -->
+        </div>
+        <div>
+          <h3>Créer un site web évolutif</h3>
+          <div>Mettre en place un design qui permet une évolution facile du site au fil du temps, pour intégrer de nouvelles fonctionnalités et suivre les tendances du marché.</div>
+          <!-- <div>Découvrir notre <nu-link label="Découvrir notre pole Webdesign" to="/webdesign">pole Webdesign</nu-link></div> -->
+        </div>
+      </div>
+    </section>
+    <section>
+      <h2>Ai-je besoin d'un service de webdesign ?</h2>
+      <div>Un site internet est un outil essentiel pour augmenter la visibilité de votre entreprise. Il vous permet de renforcer votre image de marque, gérer efficacement votre réputation en ligne, cibler de nouveaux segments de marché, fidéliser vos clients et améliorer vos échanges avec eux.</div>
+			<div>
+        <div>
+          <div>Vous souhaitez</div>
+          <ul>
+            <li>Améliorer l’expérience utilisateur</li>
+            <li>Renforcer l'image de marque</li>
+            <li>Optimiser la conversion</li>
+            <li>Assurer une compatibilité mobile</li>
+            <li>Optimiser le référencement SEO</li>
+          </ul>
+        </div>
+        <div>
+          <div>tout en</div>
+          <ul>
+            <li>Participant activement</li>
+            <li>Collaborant sur le choix du design </li>
+            <li>Etant patient pour obtenir des résultats</li>
+            <li>Etant prêt à vous démarquer</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        Alors notre équipe n'attend plus que vous pour designer vos supports web.
+        <nu-link label="Demander un devis" to="/devis">
+          <nu-button class="w-full mt-auto">
+            Demander un devis
+          </nu-button>
+        </nu-link>
+      </div>
+      <div>
+        Vous n'êtes pas sûr - <nu-link label="Contactez-nous" to="/contact">parlons-en</nu-link>
+      </div>
+    </section>
+    
+    <section>
+      <h2>Pourquoi une Agence Web pour vos Supports ?</h2>
+			<div>
+        <h3>La Vision de la Création des Supports Digitaux chez Nutshell</h3>
+        <div>
+          Chez Nutshell, la création de supports digitaux va bien au-delà du simple design. C'est un processus stratégique visant à concevoir des outils numériques qui incarnent l'identité de la marque, répondent aux besoins des utilisateurs et soutiennent les objectifs commerciaux. Chaque support, qu’il s’agisse d’un site web, d’une landing page, ou d’une application mobile, doit offrir une expérience utilisateur fluide et agréable, tout en respectant les normes de performance et de référencement. Nous croyons qu'un support digital bien conçu doit être à la fois fonctionnel et esthétique, guidant l'utilisateur dans son parcours tout en valorisant les services ou produits proposés.
+        </div>
+        <div>
+          Notre approche repose sur l’optimisation continue, où nous travaillons en étroite collaboration avec nos clients pour que chaque support soit aligné avec leurs ambitions. De la première esquisse à la mise en ligne, nous mettons un point d'honneur à créer des expériences immersives, engageantes et durables. Nous privilégions une vision à long terme, dans laquelle chaque support numérique est conçu pour évoluer avec les tendances du marché et les attentes des utilisateurs.
+        </div>
+      </div>
+			<div>
+        <h3>Une méthodologie pour réussir votre site web</h3>
+        <div>
+          <div>
+            <div>1.</div>
+            <div>Analyse et Ateliers de Co-Conception</div>
+            <ul>
+              <li>Définir les objectifs et les cibles.</li>
+              <li>Organiser des ateliers de co-conception pour structurer l’UX et l’architecture..</li>
+            </ul>
+          </div>
+          <div>
+            <div>2.</div>
+            <div>Créer et Valider le Design</div>
+            <ul>
+              <li>Créer des maquettes graphiques en accord avec l’identité visuelle.</li>
+              <li>Valider les choix esthétiques lors des échanges avec le client.</li>
+            </ul>
+          </div>
+          <div>
+            <div>3.</div>
+            <div>Concevoir les supports</div>
+            <ul>
+              <li>Créer les supports digitaux.</li>
+              <li>Intégrer les maquettes pour des pages web.</li>
+            </ul>
+          </div>
+          <div>
+            <div>4.</div>
+            <div>Livrer et Suivre</div>
+            <ul>
+              <li>Mettre en ligne après validation finale.</li>
+              <li>Livrer tous les support digitaux.</li>
+              <li>Rester à l'écoute pour tout changement ou nouveaux supports.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h3>Adopter une approche RSE pour vos projets de Webdesign</h3>
+        <div>
+          Dans un monde de plus en plus tourné vers la responsabilité sociétale, notre agence intègre une démarche RSE dans la réalisation de chaque projet de webdesign. Nous privilégions des solutions durables et éthiques, que ce soit dans le choix des technologies, l’optimisation des performances énergétiques de vos supports web, ou la création d’interfaces accessibles et inclusives. En adoptant une approche respectueuse de l’environnement et des utilisateurs, nous nous engageons à créer des sites et supports digitaux qui contribuent à un avenir plus responsable tout en répondant à vos besoins spécifiques.
+        </div>
+        <div>En savoir plus sur <nu-link label="Découvrir nos engagements" to="/nos-engagements">nos engagements</nu-link></div>
+      </div>
+    </section>
+    <section>
+      <h2>Pour aller plus loin.</h2>
+      <div>
+        <div>
+          <h3>Création de site internet</h3>
+          <div>Dotez votre entreprise d’un site web moderne et fonctionnel. Nous vous proposons une création sur mesure qui allie design, performance et expérience utilisateur pour un impact maximal.</div>
+          <nu-link label="Découvrir notre agence web" to="/creation-site-internet">
+            <nu-button class="w-full mt-auto">
+              Agence de Création de site
+            </nu-button>
+          </nu-link>
+        </div>
+        <div>
+          <h3>Identité de marque</h3>
+          <div>Créez une identité visuelle unique et percutante pour renforcer l’image de votre marque et marquer les esprits de vos clients.</div>
+          <nu-link label="Découvrir notre agence de marque" to="/identite-de-marque">
+            <nu-button class="w-full mt-auto">
+              Agence de marque 
+            </nu-button>
+          </nu-link>
+        </div>
+        <div>
+          <h3>Référencement naturel</h3>
+          <div>Optimisez votre visibilité en ligne grâce à un référencement naturel sur-mesure, pour attirer plus de visiteurs et améliorer votre positionnement sur les moteurs de recherche.</div>
+          <nu-link label="Découvrir notre agence SEO" to="/referencement-naturel">
+            <nu-button class="w-full mt-auto">
+              Agence SEO
+            </nu-button>
+          </nu-link>
+        </div>
+        <div>
+          <h3>Stratégie digitale</h3>
+          <div>Définissez une stratégie digitale sur-mesure pour maximiser votre impact en ligne, aligner vos objectifs commerciaux et atteindre vos cibles efficacement.</div>
+          <nu-link label="Découvrir notre agence digitale" to="/strategie-digitale">
+            <nu-button class="w-full mt-auto">
+              Agence digitale 
+            </nu-button>
+          </nu-link>
+        </div>
+      </div>
+    </section>
+    <section>
+      <h2>Discutons de votre projet.</h2>
+      <!-- <div>
+        TODO: form devis
+      </div> -->
+    </section>
 	</div>
 </template>
