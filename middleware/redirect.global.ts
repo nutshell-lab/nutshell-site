3c9b10de8e977d8720ea6b5e470876cd95215130
Thanith <nathan.boutte@viacesi.fr>
2025-03-05 18:18:43 +0100

    chore: comment trailling / buster

--- middleware/redirect.global.ts
+++ middleware/redirect.global.ts
@@ -1,12 +1,12 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   //TODO check if this is good for google ?
   // Redirect all trailling / to non trailling / url
-  if (to.path !== '/' && to.path.endsWith('/')) {
-    const { path, query, hash } = to;
-    const nextPath = path.replace(/\/+$/, '') || '/';
-    const nextRoute = { path: nextPath, query, hash };
-    return navigateTo(nextRoute, { redirectCode: 301 });
-  }
+  // if (to.path !== '/' && to.path.endsWith('/')) {
+  //   const { path, query, hash } = to;
+  //   const nextPath = path.replace(/\/+$/, '') || '/';
+  //   const nextRoute = { path: nextPath, query, hash };
+  //   return navigateTo(nextRoute, { redirectCode: 301 });
+  // }
   // Redirect /blog to /dossier (usefull for breadrumb component)
   if (to.path === '/blog') {
     return navigateTo('/dossiers', { redirectCode: 301 })
