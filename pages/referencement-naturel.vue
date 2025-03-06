1b2526e06a273c4fbb9b8f2e0f2ab5340f4e6514
Thanith <nathan.boutte@viacesi.fr>
2025-03-06 17:25:59 +0100

    feat: add natural ref page

--- pages/referencement-naturel.vue
+++ pages/referencement-naturel.vue
@@ -1,5 +1,226 @@
 <template>
 	<div class="bg-alabaster font-inter">
-    /devis
+    <section>
+      <h1>Agence de Référencement : Optimisez Votre Site Internet pour les Moteurs de Recherche</h1>
+			<div>Optimisez votre site internet avec notre agence de référencement. Dominez les moteurs de recherche et attirez plus de visiteurs grâce à un référencement SEO efficace !</div>
+      <div>
+        Notre agence de référencement se spécialise dans le référencement naturel SEO, une stratégie essentielle pour améliorer la visibilité de votre site internet sur Google et les autres moteurs de recherche. En optimisant votre web avec des techniques éprouvées, nous augmentons votre taux de conversion et attirons un trafic qualifié. Notre expertise en référencement naturel SEO vous assure une présence durable et efficace sur la première page de Google, renforçant ainsi votre autorité en ligne.
+      </div>
+      <nu-link label="Demander un devis" to="/devis">
+        <nu-button class="w-full mt-auto">
+          Améliorer mon référencement
+        </nu-button>
+      </nu-link>
+    </section>
+    <section>
+      <h2>Nos Clients Satisfaits par Notre Référencement Naturel SEO</h2>
+    </section>
+    <section>
+      <h2>Nos Compétences en Référencement Naturel SEO.</h2>
+			<div>Nutshell, votre partenaire en référencement naturel SEO, vous accompagne dans l'optimisation de votre site internet pour les moteurs de recherche. Grâce à notre expertise en référencement SEO, nous vous aidons à atteindre les premières pages de Google, augmentant ainsi votre taux de conversion et attirant un trafic qualifié. Explorez nos services et découvrez comment nous pouvons booster votre visibilité en ligne</div>
+			<div>
+        <div>
+          <h3>Analyse SEO</h3>
+          <div>Confiez l'audit SEO de votre site web à Nutshell pour une évaluation précise de votre référencement naturel. Nous examinons chaque aspect de votre site internet pour vous fournir des recommandations stratégiques visant à améliorer votre positionnement sur Google et les autres moteurs de recherche.</div>
+        </div>
+        <div>
+          <h3>Analyse de la Concurrence SEO</h3>
+          <div>Bénéficiez d'une analyse détaillée de la concurrence pour affiner votre stratégie de référencement naturel. Nous examinons les pratiques de vos concurrents pour vous offrir des recommandations ciblées, visant à renforcer votre visibilité sur les moteurs de recherche.</div>
+        </div>
+        <div>
+          <h3>Stratégie SEO Personnalisée</h3>
+          <div>Nutshell élabore des stratégies SEO sur mesure pour améliorer votre référencement naturel et renforcer votre présence sur Google. Nous vous aidons à définir des objectifs clairs et à mettre en œuvre des actions concrètes pour optimiser votre site internet et augmenter votre taux de conversion.</div>
+        </div>
+        <div>
+          <h3>Optimisation des Mots Clés</h3>
+          <div>Nous optimisons vos mots clés pour améliorer votre référencement naturel et attirer un trafic qualifié sur votre site internet. Nos experts identifient les termes les plus pertinents pour augmenter votre visibilité sur Google et les autres moteurs de recherche.</div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <h2>Façonnez l’image que Google a de vous.</h2>
+			<div>
+        <div>
+          <h3>Améliorer la Visibilité sur Google</h3>
+          <div>En optimisant votre site internet avec des techniques de référencement naturel SEO, vous augmentez vos chances d'apparaître en première page de Google. Une meilleure visibilité attire plus de visiteurs et renforce votre présence en ligne.</div>
+        </div>
+        <div>
+          <h3>Augmenter le Trafic Qualifié</h3>
+          <div>Un bon référencement naturel SEO permet d'attirer un trafic qualifié, composé de visiteurs réellement intéressés par vos produits ou services. Cela se traduit par un taux de conversion plus élevé et des opportunités commerciales accrues.</div>
+          <div>Voir notre <nu-link label="Découvrir notre pole Digital" to="/strategie-digitale">pole Stratégie Digitale</nu-link></div>
+        </div>
+        <div>
+          <h3>Renforcer l'Autorité de Votre Marque</h3>
+          <div>En apparaissant régulièrement dans les résultats de recherche, votre marque gagne en crédibilité et en autorité. Un référencement naturel efficace contribue à établir une image de leader dans votre secteur.</div>
+					<div>Découvrir notre <nu-link label="Découvrir notre pole Marque" to="/identite-de-marque">pole Identité de marque</nu-link></div>
+        </div>
+        <div>
+          <h3>Optimiser le Retour sur Investissement</h3>
+          <div>Le référencement naturel SEO est une stratégie à long terme qui offre un excellent retour sur investissement. Contrairement à la publicité payante, les résultats obtenus sont durables et nécessitent moins de dépenses continues.</div>
+					<div>Découvrir notre <nu-link label="Découvrir notre pole SEA" to="/referencement-payant">pole Publicité</nu-link></div>
+        </div>
+        <div>
+          <h3>Se Démarquer de la Concurrence</h3>
+          <div>En investissant dans le référencement naturel SEO, vous prenez une longueur d'avance sur vos concurrents. Une stratégie bien exécutée vous permet de capter une part plus importante du marché en ligne.</div>
+        </div>
+        <div>
+          <h3>Améliorer l'Expérience Utilisateur</h3>
+          <div>Le référencement naturel SEO implique souvent des améliorations techniques et de contenu qui rendent votre site internet plus agréable et facile à utiliser. Une meilleure expérience utilisateur favorise l'engagement et la fidélisation des visiteurs.</div>
+					<div>En savoir plus avec notre <nu-link label="Découvrir notre pole Webdesign" to="/webdesign">pole Webdesign</nu-link></div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <h2>Le Référencement Naturel est-il Indispensable pour Votre Site Internet ?</h2>
+      <div>Le référencement naturel SEO est indispensable pour optimiser la performance de votre site internet. En améliorant votre positionnement sur les moteurs de recherche, vous augmentez vos chances d'atteindre la première page de Google, renforçant ainsi votre autorité en ligne et attirant davantage de visiteurs qualifiés.</div>
+			<div>
+        <div>
+          <div>Vous avez besoin de</div>
+          <ul>
+            <li>Visibilité durable</li>
+            <li>Trafic qualifié</li>
+            <li>Crédibilité et confiance</li>
+            <li>Coût maîtrisé</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        Êtes-vous prêt à démarrer votre stratégie de référencement naturel ?
+        <nu-link label="Demander un devis" to="/devis">
+          <nu-button class="w-full mt-auto">
+            Demander un devis
+          </nu-button>
+        </nu-link>
+      </div>
+      <div>
+        Vous n'êtes pas sûr - <nu-link label="Contactez-nous" to="/contact">parlons-en ensemble</nu-link>
+      </div>
+    </section>
+    
+    <section>
+      <h2>Nutshell, votre agence de référencement pour une visibilité maximale</h2>
+			<div>
+        <h3>Notre vision du référencement naturel : un engagement durable pour votre succès</h3>
+        <div>
+          Chez Nutshell, nous croyons que le référencement naturel n’est pas une simple tactique, mais un véritable levier pour la croissance durable de votre entreprise. Notre approche va bien au-delà des simples optimisations techniques ; elle repose sur une stratégie globale et pérenne qui vise à renforcer votre positionnement sur Google et les moteurs de recherche. Nous nous engageons à vous fournir des solutions sur mesure, adaptées à vos besoins spécifiques, pour un taux de conversion optimal. Ensemble, nous construirons une présence web solide, capable de répondre aux exigences de votre marché et d'attirer un trafic qualifié, jour après jour.
+        </div>
+      </div>
+			<div>
+        <h3>Méthodologie pour une stratégie SEO réussie</h3>
+				<div>Chez Nutshell, nous adoptons une approche méthodique et sur-mesure pour maximiser les résultats de votre référencement naturel SEO. Voici les étapes clés pour mener à bien une stratégie SEO efficace.</div>
+        <div>
+          <div>
+            <div>1.</div>
+            <div>Audit SEO complet</div>
+            <ul>
+              <li>Évaluation de l’état actuel de votre site internet.</li>
+              <li>Identification des points forts et des axes d'amélioration.</li>
+              <li>Définition conjoint des objectifs SEO à atteindre.</li>
+            </ul>
+          </div>
+          <div>
+            <div>2.</div>
+            <div>Recherche de mots-clés</div>
+            <ul>
+              <li>Analyse de la concurrence et des tendances de recherche.</li>
+              <li>Sélection des mots-clés pertinents pour votre secteur d’activité.</li>
+              <li>Identification des mots-clés à fort potentiel en termes de volume et de concurrence.</li>
+            </ul>
+          </div>
+          <div>
+            <div>3.</div>
+            <div>Optimisation technique du site</div>
+            <ul>
+              <li>Vérification de la structure du site : architecture, maillage interne, etc.</li>
+              <li>Amélioration de la vitesse de chargement et de la compatibilité mobile.</li>
+              <li>Optimisation des balises HTML (title, meta description, H1, etc.) et des URLs.</li>
+              <li>Sécurisation du site (passage en HTTPS, réduction des erreurs serveur).</li>
+            </ul>
+          </div>
+          <div>
+            <div>4.</div>
+            <div>Création de contenu optimisé</div>
+            <ul>
+              <li>Rédaction de contenu unique et pertinent basé sur les mots-clés.</li>
+              <li>Mise en valeur des sujets d’intérêt pour votre audience et votre secteur.</li>
+              <li>Optimisation du contenu pour le référencement naturel SEO (intégration de mots-clés, balises, titres).</li>
+              <li>Amélioration de l’expérience utilisateur avec du contenu bien structuré et engageant.</li>
+            </ul>
+          </div>
+          <div>
+            <div>5.</div>
+            <div>Acquisition de backlinks de qualité</div>
+            <ul>
+              <li>Mise en place d'une stratégie de netlinking pour obtenir des backlinks de qualité.</li>
+              <li>Recherche de partenariats avec des sites autoritaires dans votre domaine.</li>
+              <li>Analyse des liens entrants pour renforcer l'autorité de votre site sur Google.</li>
+            </ul>
+          </div>
+          <div>
+            <div>6.</div>
+            <div>Suivi et analyse des performances</div>
+            <ul>
+              <li>Utilisation d'outils comme Google Analytics et Google Search Console pour suivre les performances.</li>
+              <li>Analyse des données sur le trafic, le taux de conversion et les positions dans les résultats de recherche.</li>
+              <li>Ajustement de la stratégie SEO en fonction des résultats obtenus et des fluctuations sur Google.</li>
+            </ul>
+          </div>
+          <div>
+            <div>7.</div>
+            <div>Rapport et ajustements réguliers</div>
+            <ul>
+              <li>Création de rapports réguliers sur les performances SEO.</li>
+              <li>Revue des objectifs, suivi des KPI (mots-clés, trafic, conversions).</li>
+              <li>Ajustements et optimisations continues de la stratégie en fonction des évolutions du marché et des algorithmes Google.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h3>L’éco-conception, un levier pour booster votre SEO</h3>
+        <div>
+          L’éco-conception et le référencement naturel SEO vont de pair pour améliorer la performance et la visibilité de votre site. En optimisant la vitesse de chargement, réduisant la taille des fichiers et simplifiant le code, vous améliorez non seulement l'expérience utilisateur mais aussi le classement sur Google. De plus, une approche éco-responsable renforce l’accessibilité du site et sa compatibilité mobile, deux critères essentiels pour un SEO réussi. Ainsi, l’éco-conception devient un véritable atout pour un SEO durable et efficace.
+        </div>
+        <div>En savoir plus sur <nu-link label="Découvrir nos engagements" to="/nos-engagements">nos engagements</nu-link></div>
+      </div>
+    </section>
+    <section>
+      <h2>Explorez l'intégration de nos services complémentaires.</h2>
+      <div>
+        <div>
+          <h3>Création de site internet</h3>
+          <div>Offrez à votre entreprise un site web à la fois moderne et performant. Nous vous proposons une création personnalisée alliant design, efficacité et expérience utilisateur pour un impact optimal.</div>
+          <nu-link label="Découvrir notre agence web" to="/creation-site-internet">
+            <nu-button class="w-full mt-auto">
+              Agence de Création de site
+            </nu-button>
+          </nu-link>
+        </div>
+        <div>
+          <h3>Stratégie digitale</h3>
+          <div>Optez pour une stratégie digitale sur mesure et boostez votre visibilité en ligne ! Nous concevons et développons du contenu parfaitement adapté à votre audience et à vos objectifs pour maximiser votre impact.</div>
+          <nu-link label="Découvrir notre agence" to="/strategie-digitale">
+            <nu-button class="w-full mt-auto">
+              Agence digitale 
+            </nu-button>
+          </nu-link>
+        </div>
+        <div>
+          <h3>Webdesign</h3>
+          <div>Optimisez la conversion de vos visiteurs en simplifiant la navigation et en suivant les meilleures pratiques du web. Un webdesign réfléchi est crucial pour améliorer votre taux de conversion.</div>
+          <nu-link label="Découvrir notre agence" to="/webdesign">
+            <nu-button class="w-full mt-auto">
+              Agence webdesign
+            </nu-button>
+          </nu-link>
+        </div>
+      </div>
+    </section>
+    <section>
+      <h2>Discutons de votre projet.</h2>
+      <!-- <div>
+        TODO: form devis
+      </div> -->
+    </section>
 	</div>
 </template>
