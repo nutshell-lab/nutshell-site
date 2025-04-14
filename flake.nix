d08cacb6429e9264357018d3a15e3715a148ae61
cprice <cprice@tutanota.com>
2025-04-14 12:20:29 +0200

    fix: [security] - complete security headers using nuxt-security module

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixvim.url = "github:NoBrainSkull/nixvim-flake?tag=24.11_v2.1.10";
+  };
+
+  outputs = { nixvim, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_20
+          pkgs.typescript
+          pkgs.typescript-language-server
+          pkgs.nixfmt
+          pkgs.pnpm
+          (nixvim.packages.${system}.default.extend {
+            plugins.lsp = {
+              enable = true;
+              servers = {
+                ts_ls.enable = true;
+                nixd = { enable = true; };
+                volar.enable = true;
+              };
+            };
+          })
+        ];
+      };
+    };
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,98 +2,99 @@ import vsharp from 'vite-plugin-vsharp';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
- devtools: { enabled: true },
+  devtools: { enabled: true },
 
- imports: {
-					presets: [
-									{
-													from: 'vee-validate',
-													imports: ['useForm']
-									}
-					]
-	},
+  imports: {
+    presets: [
+      {
+        from: 'vee-validate',
+        imports: ['useForm']
+      }
+    ]
+  },
 
- // vite: {
-	// 	plugins: [vsharp()],
-	// },
-	css: ['~/assets/css/main.css'],
+  // vite: {
+  // 	plugins: [vsharp()],
+  // },
+  css: ['~/assets/css/main.css'],
 
- modules: [
-					'@nuxtjs/plausible',
-					'@nuxtjs/tailwindcss',
-					'@nuxt/content',
-					'nuxt-simple-sitemap',
-					'nuxt-route-meta',
-					"@nuxt/image"
-	],
+  modules: [
+    '@nuxtjs/plausible',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/content',
+    'nuxt-simple-sitemap',
+    'nuxt-route-meta',
+    'nuxt-security',
+    "@nuxt/image"
+  ],
 
- plausible: {
-					domain: 'nutshell-lab.com',
-					apiHost: 'https://plausible.nutshell-lab.com'
-	},
+  plausible: {
+    domain: 'nutshell-lab.com',
+    apiHost: 'https://plausible.nutshell-lab.com'
+  },
 
- site: {
-					url: 'https://nutshell-lab.com',
-	},
+  site: {
+    url: 'https://nutshell-lab.com',
+  },
 
- content: {
-					contentHead: false
-	},
+  content: {
+    contentHead: false
+  },
 
- sitemap: {
-					strictNuxtContentPaths: true,
-					exclude: [
-									'/mentions-legales',
-									'/faq',
-									'/cgv',
-									'/services'
-					],
-	},
+  sitemap: {
+    strictNuxtContentPaths: true,
+    exclude: [
+      '/mentions-legales',
+      '/faq',
+      '/cgv',
+      '/services'
+    ],
+  },
 
- ignore: [
-					process.env.CF_PAGES == "1" ? 'pages/dev/*' : '',
-					process.env.CF_PAGES == "1" ? 'pages/faq.vue' : '',
-					process.env.CF_PAGES == "1" ? 'pages/cgv.vue' : '',
-					process.env.CF_PAGES == "1" ? 'pages/services.vue' : '',
-	],
+  ignore: [
+    process.env.CF_PAGES == "1" ? 'pages/dev/*' : '',
+    process.env.CF_PAGES == "1" ? 'pages/faq.vue' : '',
+    process.env.CF_PAGES == "1" ? 'pages/cgv.vue' : '',
+    process.env.CF_PAGES == "1" ? 'pages/services.vue' : '',
+  ],
 
- // fonts: {
-	// 	families: [
-	// 		// do not resolve this font with any provider from `@nuxt/fonts`
-	// 		{ name: 'Silka', provider: 'local' },
-	// 		// only resolve this font with the `google` provider
-	// 		{ name: 'Inter', provider: 'local' },
-	// 	],
-	// 	experimental: {
-	// 	  // You can enable support for adding preload links to the initially rendered HTML.
-	// 	  // There is a known upstream issue with rendering unaesthetic links with a `../` in the URL.
-	// 	  addPreloadLinks: true
-	// 	}
-	// },
-	app: {
-					pageTransition: { name: 'page', mode: 'out-in' },
-					head: {
-									htmlAttrs: {
-											lang: 'fr'
-									},
-									meta: [
-													{
-																	name: 'theme-color',
-																	content: '#161616',
-													},
-									],
-									link: [
-													{
-																	rel: 'icon',
-																	href: '/favicon.ico'
-													},
-													{
-																	rel: 'apple-touch-icon',
-																	href: '/apple-touch-icon.png'
-													}
-									]
-					}
-	},
+  // fonts: {
+  // 	families: [
+  // 		// do not resolve this font with any provider from `@nuxt/fonts`
+  // 		{ name: 'Silka', provider: 'local' },
+  // 		// only resolve this font with the `google` provider
+  // 		{ name: 'Inter', provider: 'local' },
+  // 	],
+  // 	experimental: {
+  // 	  // You can enable support for adding preload links to the initially rendered HTML.
+  // 	  // There is a known upstream issue with rendering unaesthetic links with a `../` in the URL.
+  // 	  addPreloadLinks: true
+  // 	}
+  // },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      htmlAttrs: {
+        lang: 'fr'
+      },
+      meta: [
+        {
+          name: 'theme-color',
+          content: '#161616',
+        },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          href: '/favicon.ico'
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: '/apple-touch-icon.png'
+        }
+      ]
+    }
+  },
 
- compatibilityDate: '2024-09-16'
+  compatibilityDate: '2024-09-16'
 })
