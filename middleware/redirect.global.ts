d77e1bf550facf6f24d5f3ccafd89a8833d58979
Thanith <nathan.boutte@viacesi.fr>
2025-05-16 14:28:07 +0200

    test deploy

--- middleware/redirect.global.ts
+++ middleware/redirect.global.ts
@@ -1,11 +1,11 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   // Redirect all trailling / to non trailling / url
-  if (to.path !== '/' && to.path.endsWith('/')) {
-    const { path, query, hash } = to;
-    const nextPath = path.replace(/\/+$/, '') || '/';
-    const nextRoute = { path: nextPath, query, hash };
-    return navigateTo(nextRoute, { redirectCode: 301 });
-  }
+  // if (to.path !== '/' && to.path.endsWith('/')) {
+  //   const { path, query, hash } = to;
+  //   const nextPath = path.replace(/\/+$/, '') || '/';
+  //   const nextRoute = { path: nextPath, query, hash };
+  //   return navigateTo(nextRoute, { redirectCode: 301 });
+  // }
   // Redirect /blog to /dossier (usefull for breadrumb component)
   if (to.path === '/blog') {
     return navigateTo('/dossiers', { redirectCode: 301 })
